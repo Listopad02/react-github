8adcd42fe58a8e1cef826f06d2f58f2a0dbb1ddf
Listopad02 <123ayetvbass@gmail.com>
2022-01-03 19:26:27 +0300

    alert component fully realized

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,21 @@
+import { useContext } from "react"
+import { AlertContext } from "../context/alert/alertContext"
+
 export const Search = () => {
+    const {show} = useContext(AlertContext)
+
+    const onSubmit = e => {
+        if (e.key === 'Enter') {
+            show('This is alert')
+        }
+    }
     
     return (
         <div className="form-group">
             <input type="text"
                    className="form-control"
-                   placeholder="Github username..." />
+                   placeholder="Github username..."
+                   onKeyPress={onSubmit} />
         </div>
     )
 }
