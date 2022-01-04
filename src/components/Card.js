2a03642d3fa33beed917825bd39a951be468ea8f
Listopad02 <123ayetvbass@gmail.com>
2022-01-04 17:13:55 +0300

    home page realized

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom"
 
-export const Card = () => (
-    <div className="card mt-3">
-        <img src={''} alt={''} className="card-image-top" />
+export const Card = ({user}) => (
+    <div className="card mt-3 h-100">
+        <img src={user.avatar_url} alt={user.login} className="card-image-top h-100" />
         <div className="card-body">
-            <h5 className="card-title">ReactJS</h5>
-            <Link to={"/profile/"} className="btn btn-primary">Open</Link>
+            <h5 className="card-title">{user.login}</h5>
+            <Link to={"/profile/" + user.login} className="btn btn-primary">Open</Link>
         </div>
     </div>
 )
--- src/components/Search.js
+++ src/components/Search.js
@@ -4,17 +4,21 @@ import { GithubContext } from "../context/github/githubContext"
 
 export const Search = () => {
     const [value, setValue] = useState('')
-    const {show} = useContext(AlertContext)
+    const alert = useContext(AlertContext)
     const github = useContext(GithubContext)
 
     const onSubmit = e => {
         if (e.key !== 'Enter') {
             return
         }
+
+        github.clearUsers()
+
         if (value.trim()) {
+            alert.hide()
             github.search(value.trim())
         } else {
-            show('Enter the Github username!')
+            alert.show('Enter the Github username!')
         }
     }
     
