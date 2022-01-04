02dfb09d9814600af9665ead235cfa17f0cf88c6
Listopad02 <123ayetvbass@gmail.com>
2022-01-04 15:56:37 +0300

    new actions for github state created

--- src/App.js
+++ src/App.js
@@ -5,22 +5,25 @@ import { Profile } from "./pages/Profile";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Alert } from "./components/Alert";
 import { AlertState } from "./context/alert/alertState";
+import { GithubState } from "./context/github/githubState";
 
 function App() {
   return (
-    <AlertState>
-      <BrowserRouter>
-      <Navbar />
-      <div className="container pt-4">
-        <Alert alert={{text: 'Test alert'}} />
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/profile/:name" element={<Profile />} />
-        </Routes>
-      </div>
-      </BrowserRouter>
-    </AlertState>
+    <GithubState>
+      <AlertState>
+        <BrowserRouter>
+        <Navbar />
+        <div className="container pt-4">
+          <Alert alert={{text: 'Test alert'}} />
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/profile/:name" element={<Profile />} />
+          </Routes>
+        </div>
+        </BrowserRouter>
+      </AlertState>
+    </GithubState>
   );
 }
 
