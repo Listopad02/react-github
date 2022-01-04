d72f8b776054a9a884ffc3d1eeef93bc4b1d4d6d
Listopad02 <123ayetvbass@gmail.com>
2022-01-04 16:13:52 +0300

    github state fully created

--- src/context/github/githubState.js
+++ src/context/github/githubState.js
@@ -1,4 +1,5 @@
 import { useReducer } from "react";
+import { CLEAR_USERS, GET_REPOS, GET_USER, SEARCH_USERS, SET_LOADING } from "../types";
 import { GithubContext } from "./githubContext";
 import { githubReducer } from "./githubReducer";
 
@@ -11,9 +12,46 @@ export const GithubState = ({children}) => {
     }
     const [state, dispatch] = useReducer(githubReducer, initialState)
 
+    const search = async value => {
+        setLoading()
+        // ...
+
+        dispatch({
+            type: SEARCH_USERS,
+            payload: []
+        })
+    }
+
+    const getUser = async name => {
+        setLoading()
+        // ...
+
+        dispatch({
+            type: GET_USER,
+            payload: {}
+        })
+    }
+
+    const getRepos = async name => {
+        setLoading()
+        // ...
+
+        dispatch({
+            type: GET_REPOS,
+            payload: []
+        })
+    }
+
+    const clearUsers = () => dispatch({ type: CLEAR_USERS })
+
+    const setLoading = () => dispatch({ type: SET_LOADING })
+
+    const { user, users, repos, loading } = state
+
     return (
         <GithubContext.Provider value={{
-
+            setLoading, search, getUser, getRepos, clearUsers,
+            user, users, repos, loading
         }}>
             { children }
         </GithubContext.Provider>
