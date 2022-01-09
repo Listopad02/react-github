573fe2177600b5da1891191f98b91cce31a56a0c
Listopad02 <123ayetvbass@gmail.com>
2022-01-09 22:34:18 +0300

    profile page have been realized

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom"
 
 export const Card = ({user}) => (
-    <div className="card mt-3 h-100">
-        <img src={user.avatar_url} alt={user.login} className="card-image-top h-100" />
+    <div className="card mt-3">
+        <img src={user.avatar_url} alt={user.login} className="card-img-top" />
         <div className="card-body">
             <h5 className="card-title">{user.login}</h5>
             <Link to={"/profile/" + user.login} className="btn btn-primary">Open</Link>
